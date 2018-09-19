e3c001245173eb5f0b1e3becffe1ce4b25e32860
Wojciech Krysiak <wojciech.g.krysiak@gmail.com>
2018-09-19 16:01:05 +0200

    debug

--- bin/bank-runner.js
+++ bin/bank-runner.js
@@ -2,38 +2,38 @@ require('dotenv').config()
 
 const TransactionsParser = require('../src/transactions-parser')
 const TransactionsFolder = require('../src/transactions-folder')
-const BankSheet = require('../src/bank-sheet')
+// const BankSheet = require('../src/bank-sheet')
 
 const run = async function() {
-  let bankSheet = new BankSheet({bankSheetId: process.env.BANK_SHEET_ID})
-  await bankSheet.init()
-  let mappings = await bankSheet.loadMappings()
-  let parsedFiles = await bankSheet.loadFiles()
+  // let bankSheet = new BankSheet({bankSheetId: process.env.BANK_SHEET_ID})
+  // await bankSheet.init()
+  // let mappings = await bankSheet.loadMappings()
+  // let parsedFiles = await bankSheet.loadFiles()
 
-  let transactionsFolder = new TransactionsFolder({
-    folderId: process.env.FOLDER_ID,
-    parsedFiles
-  })
+  // let transactionsFolder = new TransactionsFolder({
+  //   folderId: process.env.FOLDER_ID,
+  //   parsedFiles
+  // })
 
-  await transactionsFolder.init()
-  let files = await transactionsFolder.filesToImport()
+  // await transactionsFolder.init()
+  // let files = await transactionsFolder.filesToImport()
 
-  await Promise.all(files.map(async (file) => {
-    let parser = new TransactionsParser({
-      mappings,
-      fileId: file.id
-    })
-    await parser.init()
+  // await Promise.all(files.map(async (file) => {
+  //   let parser = new TransactionsParser({
+  //     mappings,
+  //     fileId: file.id
+  //   })
+  //   await parser.init()
 
-    let transactions = await parser.parse()
-    let month = transactions[transactions.length-1].accountingDate.format('MM-YYYY')
-    transactions = await bankSheet.storeTransactions({transactions, sheet: month})
-    await bankSheet.storeFile({
-      fileName: file.name,
-      fileId: file.id,
-      month: month
-    })
-  }))
+  //   let transactions = await parser.parse()
+  //   let month = transactions[transactions.length-1].accountingDate.format('MM-YYYY')
+  //   transactions = await bankSheet.storeTransactions({transactions, sheet: month})
+  //   await bankSheet.storeFile({
+  //     fileName: file.name,
+  //     fileId: file.id,
+  //     month: month
+  //   })
+  // }))
 }
 
 module.exports = run
